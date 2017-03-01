0dc6e6ffcf6ac055ce7ba15f477035eabba43e7a
deadinteger <deadinteger@gmail.com>
2017-03-01 15:42:51 -0500

    Started Somnus

--- somnusQ/somnus.js
+++ somnusQ/somnus.js
@@ -1,13 +1,17 @@
 const discord = require('discord.js');
 const client = new discord.Client();
 
+function commandIs(str,arg){
+    return msg.content.toLowerCase.startsWith("%" + str);
+}
+
 client.on('ready', () =>{
 console.log('Somnus has awoken');
 });
 
 client.on('message', message =>{
-    if(message.content === '!Test'){
-        message.reply('Somnus is Fully Operational' + ' ' + message.author.username);
+    if(message.content === 'test', message){
+        message.reply('Somnus is Fully Operational');
     }
 })
 

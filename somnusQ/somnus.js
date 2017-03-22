d634270fc78f15603fddcc941112d9ee3b052d99
deadinteger <deadinteger@gmail.com>
2017-03-22 14:10:14 -0400

    Revert "Started Somnus"
    This reverts commit 0dc6e6ffcf6ac055ce7ba15f477035eabba43e7a.

--- somnusQ/somnus.js
+++ somnusQ/somnus.js
@@ -1,17 +1,13 @@
 const discord = require('discord.js');
 const client = new discord.Client();
 
-function commandIs(str,arg){
-    return msg.content.toLowerCase.startsWith("%" + str);
-}
-
 client.on('ready', () =>{
 console.log('Somnus has awoken');
 });
 
 client.on('message', message =>{
-    if(message.content === 'test', message){
-        message.reply('Somnus is Fully Operational');
+    if(message.content === '!Test'){
+        message.reply('Somnus is Fully Operational' + ' ' + message.author.username);
     }
 })
 

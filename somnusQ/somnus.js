878c876a2855e29c9bd21ec39c2a2aba2740a2a5
deadinteger <deadinteger@gmail.com>
2017-03-22 19:41:14 -0400

    Transfer to Master PC

--- somnusQ/somnus.js
+++ somnusQ/somnus.js
@@ -1,15 +1,32 @@
 const discord = require('discord.js');
-const client = new discord.Client();
+const bot = new discord.Client();
 
-client.on('ready', () =>{
-console.log('Somnus has awoken');
+const prefix = "[]";
+
+bot.on('ready', () => {
+    console.log('Somnus has awoken');
 });
 
-client.on('message', message =>{
-    if(message.content === '!Test'){
-        message.reply('Somnus is Fully Operational' + ' ' + message.author.username);
+bot.on('message', message => {
+    
+    if(!message.content.startsWith(prefix)) {
+        
+        if(message.content.includes("cut")){
+            if(message.content.includes("myself")){
+                message.delete(100);
+               message.channel.fetchMessages({limit :3}).then(messages => message.channel.bulkDelete(messages));
+                message.channel.sendMessage( " not recognized as a command!").then((message => message.delete(500)));
+            }
     }
-})
+
+    }; // Makes sure the bot doesn't react to irrelavent stuff
+    if (message.author.bot) return; // Make sure the bot doesn't talk to itself like a loon.
+    
+   
+    if(message.content.startsWith(prefix + "test")){ 
+        message.reply("Somnus is Fully Operational."); //To see if it is on.
+    }     
+});
 
 
-client.login('Mjg2NTg0NjkzNzUzNzc0MDgz.C5i2WA.TLXRyrgczINkOdV90_19zVKG_-Y');
+bot.login('Mjg2NTg0NjkzNzUzNzc0MDgz.C5i2WA.TLXRyrgczINkOdV90_19zVKG_-Y');
